a4d4c56d348c1db3829e58816b75e562e5e37e83
ABRIN C SAIJU <60190836+abruu@users.noreply.github.com>
2020-09-02 15:19:44 +0530

    contact validation

--- assets/js/connect.js
+++ assets/js/connect.js
@@ -2,9 +2,20 @@ const name=document.getElementById("name")
 const email=document.getElementById("email")
 const msg=document.getElementById("msg")
 const submit=document.getElementById("submit")
-
+const contact1=document.getElementById("contact")
 const database = firebase.database()
 
+
+contact.addEventListener('input',()=>{
+    if(name.value.lenght>3 && email.value.lenght>0 ){
+        submit.setAttribute('disabled',);
+
+    }else{
+        submit.removeAttribute('disabled','disabled')
+    }
+}
+
+)
 submit.addEventListener('click',(e)=>{
     e.preventDefault();
 
